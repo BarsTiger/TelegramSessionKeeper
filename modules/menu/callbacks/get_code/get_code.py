dcc45e1521e1e45bf20b9d14d012356ab1e9308e
BarsTiger <kit.barsic@gmail.com>
2023-05-11 13:44:00 +0300

    Getting code works

--- modules/menu/callbacks/get_code/get_code.py
+++ modules/menu/callbacks/get_code/get_code.py
@@ -1,6 +1,19 @@
 from modules.decorators.callback import async_callback
+from modules.client import GeneratedClient
+from rich import print
 
 
 @async_callback
 async def get_code_callback(session_name: str):
-    ...
+    print('Getting messages...')
+    client = GeneratedClient(session_name)
+
+    await client.start()
+    async for message in client.get_chat_history(777000, limit=3):
+        print(message.date)
+        print(message.text)
+        print()
+
+    await client.stop()
+
+    input()
