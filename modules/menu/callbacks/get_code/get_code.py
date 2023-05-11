aadb0d6f0861662c2ae39e7792f97f3d2ce7d973
BarsTiger <kit.barsic@gmail.com>
2023-05-11 22:14:48 +0300

    Code getting with regex

--- modules/menu/callbacks/get_code/get_code.py
+++ modules/menu/callbacks/get_code/get_code.py
@@ -1,6 +1,8 @@
 from modules.decorators.callback import async_callback
 from modules.client import GeneratedClient
 from rich import print
+import os
+import re
 
 
 @async_callback
@@ -9,10 +11,24 @@ async def get_code_callback(session_name: str):
     client = GeneratedClient(session_name)
 
     await client.start()
-    async for message in client.get_chat_history(777000, limit=3):
+
+    messages = [item async for item in client.get_chat_history(777000, limit=3)]
+
+    for message in messages:
+        try:
+            group = re.search(r': (\d{5})\D', message.text).groups()[0]
+            print(f"Confirmation code: \n{group}")
+            break
+        except Exception as e:
+            assert e
+            continue
+
+    print('Last messages from Telegram: ')
+    for message in messages:
+        print('-' * os.get_terminal_size().columns)
         print(message.date)
         print(message.text)
-        print()
+    print('-' * os.get_terminal_size().columns)
 
     await client.stop()
 
